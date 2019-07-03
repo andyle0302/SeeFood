e6b5cffb7176633a29bc988b41890da17729958b
qtuter1997 <nh0k.0k.911@gmail.com>
2019-07-03 13:08:50 +0900

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Cygnus on 7/3/19.
+//  Copyright © 2019 KSS Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
